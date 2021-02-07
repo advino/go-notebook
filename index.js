a0d7e8fdb84a3853a76903ba96f404a17472190f
Advait Kalakkad <advait.kalakkad@gmail.com>
2021-02-06 20:36:02 -0800

    updates tp Stone and Board classes

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ let BOARD = require('./Board.js');
 
 let size = parseInt(process.argv[2]);
 
+//default to 19x19 board if no size is provided
+if(isNaN(size)) {
+    size = 19;
+}
+
 let game = new BOARD(size);
 game.initializeBoard();
 
